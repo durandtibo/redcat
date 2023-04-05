e298edf0fc11ef180be6f926547b736a8c640ec2
Thibaut Durand <durand.tibo@gmail.com>
2023-04-05 08:41:54 -0700

    Add some conversion operations (#5)

--- src/redcat/utils.py
+++ src/redcat/utils.py
@@ -1,4 +1,4 @@
-__all__ = ["DeviceType", "IndexType"]
+__all__ = ["DeviceType", "IndexType", "get_available_devices"]
 
 from collections.abc import Sequence
 from typing import Union
@@ -8,3 +8,24 @@ from torch import Tensor
 
 DeviceType = Union[torch.device, str, int]
 IndexType = Union[None, int, slice, str, Tensor, Sequence]
+
+
+
+def get_available_devices() -> tuple[str, ...]:
+    r"""Gets the available PyTorch devices on the machine.
+
+    Returns
+    -------
+        tuple: The available devices.
+
+    Example usage:
+
+    .. code-block:: python
+
+        >>> from redcat.utils import get_available_devices
+        >>> get_available_devices()
+        ('cpu', 'cuda:0')
+    """
+    if torch.cuda.is_available():
+        return ("cpu", "cuda:0")
+    return ("cpu",)
