c989e69602f2923269b70dec7f0f496f2ddf5ca8
Thibaut Durand <durand.tibo@gmail.com>
2023-04-07 12:35:49 -0700

    Add `align_to_batch_first`

--- src/redcat/utils.py
+++ src/redcat/utils.py
@@ -1,4 +1,4 @@
-__all__ = ["DeviceType", "IndexType", "get_available_devices"]
+__all__ = ["DeviceType", "IndexType", "align_to_batch_first", "get_available_devices"]
 
 from collections.abc import Sequence
 from typing import Union
@@ -8,6 +8,38 @@ from torch import Tensor
 
 DeviceType = Union[torch.device, str, int]
 IndexType = Union[None, int, slice, str, Tensor, Sequence]
+
+
+def align_to_batch_first(tensor: Tensor, batch_dim: int) -> Tensor:
+    r"""Aligns the input tensor format to ``(batch_size, *)`` where `*` means
+    any number of dimensions.
+
+    Args:
+        tensor (``torch.Tensor``): Specifies the tensor to change
+            format.
+        batch_dim (int): Specifies the batch dimension in the input
+            tensor.
+
+    Returns:
+        ``torch.Tensor``: A tensor of shape ``(batch_size, *)`` where
+            `*` means any number of dimensions.
+
+    Example usage:
+
+    .. code-block:: python
+
+        >>> import torch
+        >>> from redcat.utils import align_to_batch_first
+        >>> align_to_batch_first(torch.arange(20).view(4, 5), batch_dim=1)
+        tensor([[ 0,  5, 10, 15],
+                [ 1,  6, 11, 16],
+                [ 2,  7, 12, 17],
+                [ 3,  8, 13, 18],
+                [ 4,  9, 14, 19]])
+    """
+    if batch_dim == 0:
+        return tensor
+    return tensor.transpose(0, batch_dim)
 
 
 def get_available_devices() -> tuple[str, ...]:
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,6 +1,39 @@
 from unittest.mock import patch
 
-from redcat.utils import get_available_devices
+import torch
+
+from redcat.utils import align_to_batch_first, get_available_devices
+
+##########################################
+#     Tests for align_to_batch_first     #
+##########################################
+
+
+def test_align_to_batch_first_no_permutation() -> None:
+    assert align_to_batch_first(torch.arange(10).view(5, 2), batch_dim=0).equal(
+        torch.tensor([[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]])
+    )
+
+
+def test_align_to_batch_first_permute_dims() -> None:
+    assert align_to_batch_first(torch.arange(10).view(5, 2), batch_dim=1).equal(
+        torch.tensor([[0, 2, 4, 6, 8], [1, 3, 5, 7, 9]])
+    )
+
+
+def test_align_to_batch_first_permute_dims_extra_dims() -> None:
+    assert align_to_batch_first(torch.arange(20).view(2, 5, 2), batch_dim=1).equal(
+        torch.tensor(
+            [
+                [[0, 1], [10, 11]],
+                [[2, 3], [12, 13]],
+                [[4, 5], [14, 15]],
+                [[6, 7], [16, 17]],
+                [[8, 9], [18, 19]],
+            ]
+        )
+    )
+
 
 ###########################################
 #     Tests for get_available_devices     #
