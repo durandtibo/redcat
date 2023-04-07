29de74605f81d22a7f22a0153f432b90bf0591c8
Thibaut Durand <durand.tibo@gmail.com>
2023-04-07 13:03:12 -0700

    Add `align_to_batch_seq` and `align_to_seq_batch` (#29)

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -6,6 +6,8 @@ from pytest import raises
 
 from redcat.utils import (
     align_to_batch_first,
+    align_to_batch_seq,
+    align_to_seq_batch,
     compute_batch_seq_permutation,
     get_available_devices,
     swap2,
@@ -37,6 +39,65 @@ def test_align_to_batch_first_permute_dims_extra_dims() -> None:
                 [[4, 5], [14, 15]],
                 [[6, 7], [16, 17]],
                 [[8, 9], [18, 19]],
+            ]
+        )
+    )
+
+
+########################################
+#     Tests for align_to_batch_seq     #
+########################################
+
+
+def test_align_to_batch_seq_no_permutation() -> None:
+    assert align_to_batch_seq(torch.arange(10).view(5, 2), batch_dim=0, seq_dim=1).equal(
+        torch.tensor([[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]])
+    )
+
+
+def test_align_to_batch_seq_permute_dims() -> None:
+    assert align_to_batch_seq(torch.arange(10).view(5, 2), batch_dim=1, seq_dim=0).equal(
+        torch.tensor([[0, 2, 4, 6, 8], [1, 3, 5, 7, 9]])
+    )
+
+
+def test_align_to_batch_seq_permute_dims_extra_dims() -> None:
+    assert align_to_batch_seq(torch.arange(20).view(2, 5, 2), batch_dim=1, seq_dim=2).equal(
+        torch.tensor(
+            [
+                [[0, 10], [1, 11]],
+                [[2, 12], [3, 13]],
+                [[4, 14], [5, 15]],
+                [[6, 16], [7, 17]],
+                [[8, 18], [9, 19]],
+            ]
+        )
+    )
+
+
+########################################
+#     Tests for align_to_seq_batch     #
+########################################
+
+
+def test_align_to_seq_batch_no_permutation() -> None:
+    assert align_to_seq_batch(torch.arange(10).view(2, 5), batch_dim=1, seq_dim=0).equal(
+        torch.tensor([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]])
+    )
+
+
+def test_align_to_seq_batch_permute_dims() -> None:
+    assert align_to_seq_batch(torch.arange(10).view(2, 5), batch_dim=0, seq_dim=1).equal(
+        torch.tensor([[0, 5], [1, 6], [2, 7], [3, 8], [4, 9]])
+    )
+
+
+def test_align_to_seq_batch_permute_dims_extra_dims() -> None:
+    assert align_to_seq_batch(torch.arange(20).view(2, 5, 2), batch_dim=1, seq_dim=2).equal(
+        torch.tensor(
+            [
+                [[0, 10], [2, 12], [4, 14], [6, 16], [8, 18]],
+                [[1, 11], [3, 13], [5, 15], [7, 17], [9, 19]],
             ]
         )
     )
