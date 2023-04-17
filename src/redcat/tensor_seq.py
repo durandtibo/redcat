1dc6715bd00053ad1884917bbf2fa68d9fd38913
Thibaut Durand <durand.tibo@gmail.com>
2023-04-16 21:09:26 -0700

    Add `max_along_seq` to `BatchedTensorSeq` (#77)

--- src/redcat/tensor_seq.py
+++ src/redcat/tensor_seq.py
@@ -9,6 +9,7 @@ import torch
 from torch import Tensor
 
 from redcat.base import BaseBatchedTensor
+from redcat.tensor import BatchedTensor
 from redcat.utils import check_batch_dims, check_seq_dims, get_batch_dims, get_seq_dims
 
 
@@ -449,6 +450,43 @@ class BatchedTensorSeq(BaseBatchedTensor):
     ################################
     #     Reduction operations     #
     ################################
+
+    def max_along_seq(self, keepdim: bool = False) -> BatchedTensor | BatchedTensorSeq:
+        r"""Computes the maximum values along the sequence dimension.
+
+        Args:
+            keepdim (bool): Indicates whether the output tensor has
+                the sequence dimension retained or not. If ``False``
+                the returned type is ``BatchedTensor``, otherwise it
+                is ``BatchedTensorSeq``. Default: ``False``
+
+        Returns:
+            ``BatchedTensor``: A batch with the maximum values along the
+                sequence dimension.
+
+        Example usage:
+
+        .. code-block:: python
+
+            >>> import torch
+            >>> from redcat import BatchedTensorSeq
+            >>> BatchedTensorSeq(torch.arange(10).view(2, 5)).max_along_seq()
+            (tensor([4, 9], batch_dim=0), tensor([4, 4], batch_dim=0))
+            >>> BatchedTensorSeq(torch.arange(30).view(2, 5, 3)).max_along_seq()
+            (tensor([[[12, 13, 14]], [[27, 28, 29]]], batch_dim=0, seq_dim=1),
+             tensor([[[4, 4, 4]], [[4, 4, 4]]], batch_dim=0, seq_dim=1))
+        """
+        values, indices = torch.max(self._data, dim=self._seq_dim, keepdim=keepdim)
+        if keepdim:
+            return (
+                BatchedTensorSeq(data=values, **self._get_kwargs()),
+                BatchedTensorSeq(data=indices, **self._get_kwargs()),
+            )
+        batch_dim = self._batch_dim if self._seq_dim > self._batch_dim else self._batch_dim - 1
+        return (
+            BatchedTensor(data=values, batch_dim=batch_dim),
+            BatchedTensor(data=indices, batch_dim=batch_dim),
+        )
 
     def _get_kwargs(self) -> dict:
         return {"batch_dim": self._batch_dim, "seq_dim": self._seq_dim}
