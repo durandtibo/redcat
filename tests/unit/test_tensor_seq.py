26b2892a5c2b4c0fc0f4ff4212177d4ad9786886
Thibaut Durand <durand.tibo@gmail.com>
2023-04-16 19:07:28 -0700

    Add `cumsum_` to `BatchedTensor(Seq)` (#76)

--- tests/unit/test_tensor_seq.py
+++ tests/unit/test_tensor_seq.py
@@ -1762,6 +1762,22 @@ def test_batched_tensor_seq_cumsum_dtype() -> None:
     )
 
 
+def test_batched_tensor_seq_cumsum_() -> None:
+    batch = BatchedTensorSeq(torch.arange(10).view(2, 5))
+    batch.cumsum_(dim=0)
+    assert batch.equal(BatchedTensorSeq(torch.tensor([[0, 1, 2, 3, 4], [5, 7, 9, 11, 13]])))
+
+
+def test_batched_tensor_seq_cumsum__custom_dims() -> None:
+    batch = BatchedTensorSeq.from_seq_batch(torch.arange(10).view(5, 2))
+    batch.cumsum_(dim=1)
+    assert batch.equal(
+        BatchedTensorSeq(
+            torch.tensor([[0, 1], [2, 5], [4, 9], [6, 13], [8, 17]]), seq_dim=0, batch_dim=1
+        )
+    )
+
+
 def test_batched_tensor_seq_cumsum_along_batch() -> None:
     assert (
         BatchedTensorSeq(torch.arange(10).view(2, 5))
