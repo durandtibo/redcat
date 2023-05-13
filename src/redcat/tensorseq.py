3b8df7b2e8682610ef3f312115702415a24c25d2
Thibaut Durand <durand.tibo@gmail.com>
2023-05-13 12:59:40 -0700

    Improve `min_along_seq` to `BatchedTensorSeq`

--- src/redcat/tensorseq.py
+++ src/redcat/tensorseq.py
@@ -587,20 +587,17 @@ class BatchedTensorSeq(BatchedTensor):
     #     Reduction operations     #
     ################################
 
-    def max_along_seq(
-        self, keepdim: bool = False
-    ) -> tuple[BatchedTensor | BatchedTensorSeq, BatchedTensor | BatchedTensorSeq]:
+    def max_along_seq(self, keepdim: bool = False) -> torch.return_types.max:
         r"""Computes the maximum values along the sequence dimension.
 
         Args:
             keepdim (bool): Indicates whether the output tensor has
-                the sequence dimension retained or not. If ``False``
-                the returned type is ``BatchedTensor``, otherwise it
-                is ``BatchedTensorSeq``. Default: ``False``
+                the sequence dimension retained or not.
+                Default: ``False``
 
         Returns:
-            ``BatchedTensor``: A batch with the maximum values along the
-                sequence dimension.
+            ``torch.return_types.max``: A batch with
+                the maximum values along the sequence dimension.
 
         Example usage:
 
@@ -609,22 +606,15 @@ class BatchedTensorSeq(BatchedTensor):
             >>> import torch
             >>> from redcat import BatchedTensorSeq
             >>> BatchedTensorSeq(torch.arange(10).view(2, 5)).max_along_seq()
-            (tensor([4, 9], batch_dim=0), tensor([4, 4], batch_dim=0))
-            >>> BatchedTensorSeq(torch.arange(30).view(2, 5, 3)).max_along_seq(keepdim=True)
-            (tensor([[[12, 13, 14]], [[27, 28, 29]]], batch_dim=0, seq_dim=1),
-             tensor([[[4, 4, 4]], [[4, 4, 4]]], batch_dim=0, seq_dim=1))
+            torch.return_types.max(
+            values=tensor([4, 9]),
+            indices=tensor([4, 4]))
+            >>> BatchedTensorSeq(torch.arange(10).view(2, 5)).max_along_seq(keepdim=True)
+            torch.return_types.max(
+            values=tensor([[4], [9]]),
+            indices=tensor([[4], [4]]))
         """
-        values, indices = torch.max(self._data, dim=self._seq_dim, keepdim=keepdim)
-        if keepdim:
-            return (
-                BatchedTensorSeq(data=values, **self._get_kwargs()),
-                BatchedTensorSeq(data=indices, **self._get_kwargs()),
-            )
-        batch_dim = self._batch_dim if self._seq_dim > self._batch_dim else self._batch_dim - 1
-        return (
-            BatchedTensor(data=values, batch_dim=batch_dim),
-            BatchedTensor(data=indices, batch_dim=batch_dim),
-        )
+        return self.max(dim=self._seq_dim, keepdim=keepdim)
 
     def mean_along_seq(self, keepdim: bool = False) -> BatchedTensor | BatchedTensorSeq:
         r"""Computes the mean values along the sequence dimension.
@@ -679,9 +669,7 @@ class BatchedTensorSeq(BatchedTensor):
         """
         return self.median(dim=self._seq_dim, keepdim=keepdim)
 
-    def min_along_seq(
-        self, keepdim: bool = False
-    ) -> torch.return_types.min:
+    def min_along_seq(self, keepdim: bool = False) -> torch.return_types.min:
         r"""Computes the minimum values along the sequence dimension.
 
         Args:
