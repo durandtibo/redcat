09edbec43b0c05f3910825a7a800bdaf2020cafe
Thibaut Durand <durand.tibo@gmail.com>
2023-05-14 21:15:06 -0700

    Rename `batchlist` package (#245)

--- src/redcat/__init__.py
+++ src/redcat/__init__.py
@@ -7,6 +7,6 @@ __all__ = [
 
 from redcat import comparators  # noqa: F401
 from redcat.base import BaseBatch
-from redcat.list import BatchList
+from redcat.batchlist import BatchList
 from redcat.tensor import BatchedTensor
 from redcat.tensorseq import BatchedTensorSeq
