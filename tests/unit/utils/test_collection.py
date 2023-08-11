980fa72f9ba80517c8bd6aab8c5ab6c1d3f72c2d
Thibaut Durand <durand.tibo@gmail.com>
2023-08-11 11:32:42 -0700

    Add `to_list` (#378)
    * Add `to_list`
    
    * Add `to_list`

---
+++ tests/unit/utils/test_collection.py
@@ -0,0 +1,34 @@
+import numpy as np
+import torch
+from coola.testing import numpy_available, torch_available
+from pytest import mark
+
+from redcat import BatchList
+from redcat.utils.collection import to_list
+
+#############################
+#     Tests for to_list     #
+#############################
+
+
+@mark.parametrize("data", ([], [1, 2, 3], ["a", "b", "c", "d"]))
+def test_to_list_list(data: list) -> None:
+    assert to_list(data) is data
+
+
+def test_to_list_tuple() -> None:
+    assert to_list((1, 2, 3)) == [1, 2, 3]
+
+
+@numpy_available
+def test_to_list_array() -> None:
+    assert to_list(np.array([1, 2, 3])) == [1, 2, 3]
+
+
+@torch_available
+def test_to_list_tensor() -> None:
+    assert to_list(torch.tensor([1, 2, 3])) == [1, 2, 3]
+
+
+def test_to_list_batch() -> None:
+    assert to_list(BatchList([1, 2, 3])) == [1, 2, 3]
