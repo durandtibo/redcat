5bb53d7a362da973f82efaa3bd95017cf412ce08
Thibaut Durand <durand.tibo@gmail.com>
2023-08-10 21:20:12 -0700

    Improve `SortReturnType` (#372)

--- src/redcat/types.py
+++ src/redcat/types.py
@@ -11,7 +11,7 @@ from unittest.mock import Mock
 from black import Any
 from coola import objects_are_equal
 from coola.types import Tensor, ndarray
-from coola.utils import is_numpy_available, is_torch_available
+from coola.utils import is_numpy_available, is_torch_available, str_indent, str_mapping
 
 if is_numpy_available():
     import numpy as np
@@ -44,3 +44,7 @@ class SortReturnType(namedtuple("SortReturnType", ["values", "indices"])):
 
     def __ne__(self, other: Any) -> bool:
         return not self.__eq__(other)
+
+    def __repr__(self) -> str:
+        args = str_indent(str_mapping({"values": self.values, "indices": self.indices}))
+        return f"{self.__class__.__qualname__}(\n  {args}\n)"
