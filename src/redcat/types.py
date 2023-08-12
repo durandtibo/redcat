8e71c66b2600c6bf5b977249475925db095adcd3
Thibaut Durand <durand.tibo@gmail.com>
2023-08-12 00:14:52 -0700

    Remove `SortReturnType`

--- src/redcat/types.py
+++ src/redcat/types.py
@@ -1,17 +1,14 @@
 from __future__ import annotations
 
-__all__ = ["IndexType", "IndicesType", "RNGType", "SortReturnType"]
+__all__ = ["IndexType", "IndicesType", "RNGType"]
 
 import random
-from collections import namedtuple
 from collections.abc import Sequence
 from typing import TypeVar, Union
 from unittest.mock import Mock
 
-from black import Any
-from coola import objects_are_equal
 from coola.types import Tensor, ndarray
-from coola.utils import is_numpy_available, is_torch_available, str_indent, str_mapping
+from coola.utils import is_numpy_available, is_torch_available
 
 if is_numpy_available():
     import numpy as np
@@ -31,19 +28,3 @@ IndexType = Union[int, slice, list[int], Tensor, None]
 IndicesType = Union[Sequence[int], Tensor, ndarray]
 
 RNGType = Union[random.Random, np.random.Generator, torch.Generator]
-
-
-class SortReturnType(namedtuple("SortReturnType", ["values", "indices"])):
-    def __eq__(self, other: Any) -> bool:
-        if not isinstance(other, SortReturnType):
-            return False
-        return objects_are_equal(self.values, other.values) and objects_are_equal(
-            self.indices, other.indices
-        )
-
-    def __ne__(self, other: Any) -> bool:
-        return not self.__eq__(other)
-
-    def __repr__(self) -> str:
-        args = str_indent(str_mapping({"values": self.values, "indices": self.indices}))
-        return f"{self.__class__.__qualname__}(\n  {args}\n)"
