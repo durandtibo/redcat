4e7b67e230bb4bbc8c05e172d6138ee5fff568ae
Thibaut Durand <durand.tibo@gmail.com>
2023-10-09 12:39:28 -0700

    Update dependencies

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
 __all__ = ["torch_greater_equal_1_13", "TORCH_GREATER_EQUAL_1_13"]
 
-import torch
-from packaging.version import Version
+import operator
+
+from feu import compare_version
 from pytest import mark
 
-TORCH_GREATER_EQUAL_1_13 = Version(torch.__version__) >= Version("1.13.0")
+TORCH_GREATER_EQUAL_1_13 = compare_version("torch", operator.ge, "1.13.0")
 
 torch_greater_equal_1_13 = mark.skipif(
     not TORCH_GREATER_EQUAL_1_13, reason="Requires torch>=1.13.0"
