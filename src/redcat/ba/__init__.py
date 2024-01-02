60ff3a02b612f050ef7c52bffc8ce1de2de819a9
Thibaut Durand <durand.tibo@gmail.com>
2024-01-01 16:13:38 -0800

    Add `check_data_and_axis`

--- src/redcat/ba/__init__.py
+++ src/redcat/ba/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-__all__ = ["check_same_batch_axis", "get_batch_axes"]
+__all__ = ["check_same_batch_axis", "check_data_and_axis", "get_batch_axes"]
 
-from redcat.ba.utils import check_same_batch_axis, get_batch_axes
+from redcat.ba.utils import check_data_and_axis, check_same_batch_axis, get_batch_axes
