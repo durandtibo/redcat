927e98746e4339ccd946c954506d6cd4ea76dd55
Thibaut Durand <durand.tibo@gmail.com>
2024-01-02 22:29:58 -0800

    Add `arrays_share_data`

--- src/redcat/ba/utils.py
+++ src/redcat/ba/utils.py
@@ -1,11 +1,46 @@
 from __future__ import annotations
 
-__all__ = ["check_same_batch_axis", "check_data_and_axis", "get_batch_axes"]
+__all__ = [
+    "arrays_share_data",
+    "check_data_and_axis",
+    "check_same_batch_axis",
+    "get_batch_axes",
+    "get_data_base",
+]
 
 from collections.abc import Iterable, Mapping
 from typing import Any
 
 from numpy import ndarray
+
+
+def arrays_share_data(x: ndarray, y: ndarray) -> bool:
+    r"""Indicate if two arrays share the same data.
+
+    Args:
+        x: Specifies the first array.
+        y: Specifies the second array.
+
+    Returns:
+        ``True`` if the two arrays share the same data, otherwise ``False``.
+
+    Example usage:
+
+    ```pycon
+    >>> from redcat import ba
+    >>> from redcat.ba import arrays_share_data
+    >>> x = ba.ones((2, 3))
+    >>> arrays_share_data(x, x)
+    True
+    >>> arrays_share_data(x, x.copy())
+    False
+    >>> y = x[1:]
+    >>> arrays_share_data(x, y)
+    True
+
+    ```
+    """
+    return get_data_base(x) is get_data_base(y)
 
 
 def check_same_batch_axis(axes: set[int]) -> None:
@@ -70,12 +105,11 @@ def get_batch_axes(args: Iterable[Any], kwargs: Mapping[str, Any] | None = None)
     Example usage:
 
     ```pycon
-    >>> import numpy as np
-    >>> import torch
-    >>> from redcat.ba import BatchedArray, get_batch_axes
+    >>> from redcat import ba
+    >>> from redcat.ba import get_batch_axes
     >>> get_batch_axes(
-    ...     args=(BatchedArray(torch.ones(2, 3)), BatchedArray(torch.ones(2, 6))),
-    ...     kwargs={"batch": BatchedArray(torch.ones(2, 4))},
+    ...     args=(ba.ones((2, 3)), ba.ones((2, 6))),
+    ...     kwargs={"batch": ba.ones((2, 4))},
     ... )
     {0}
 
@@ -85,3 +119,33 @@ def get_batch_axes(args: Iterable[Any], kwargs: Mapping[str, Any] | None = None)
     axes = {val.batch_axis for val in args if hasattr(val, "batch_axis")}
     axes.update({val.batch_axis for val in kwargs.values() if hasattr(val, "batch_axis")})
     return axes
+
+
+def get_data_base(array: ndarray) -> ndarray:
+    r"""Return the base array that owns the actual data.
+
+    Args:
+        array: Specifies the input array.
+
+    Returns:
+        The array that owns the actual data.
+
+    Example usage:
+
+    ```pycon
+    >>> from redcat import ba
+    >>> from redcat.ba import get_data_base
+    >>> x = ba.ones((2, 3))
+    >>> get_data_base(x)
+    array([[1., 1., 1.],
+           [1., 1., 1.]])
+    >>> y = x[1:]
+    >>> get_data_base(y)
+    array([[1., 1., 1.],
+           [1., 1., 1.]])
+
+    ```
+    """
+    while isinstance(array.base, ndarray):
+        array = array.base
+    return array
--- tests/unit/ba/test_utils.py
+++ tests/unit/ba/test_utils.py
@@ -3,12 +3,34 @@ from __future__ import annotations
 import numpy as np
 import pytest
 
+from redcat import ba
 from redcat.ba import (
-    BatchedArray,
+    arrays_share_data,
     check_data_and_axis,
     check_same_batch_axis,
     get_batch_axes,
+    get_data_base,
 )
+
+#######################################
+#     Tests for arrays_share_data     #
+#######################################
+
+
+def test_arrays_share_data_true() -> None:
+    x = ba.ones((2, 3))
+    assert arrays_share_data(x, x)
+
+
+def test_arrays_share_data_true_slice() -> None:
+    x = ba.ones((2, 3))
+    assert arrays_share_data(x, x[1:])
+
+
+def test_arrays_share_data_false() -> None:
+    x = ba.ones((2, 3))
+    assert not arrays_share_data(x, x.copy())
+
 
 ###########################################
 #     Tests for check_same_batch_axis     #
@@ -56,18 +78,34 @@ def test_check_data_and_axis_incorrect_batch_axis(array: np.ndarray, batch_axis:
 
 
 def test_get_batch_axes_1_array() -> None:
-    assert get_batch_axes(
-        (BatchedArray(np.ones((2, 3))), BatchedArray(np.ones((2, 3)))),
-        {"val": BatchedArray(np.ones((2, 3)))},
-    ) == {0}
+    assert get_batch_axes((ba.ones((2, 3)), ba.ones(2)), {"batch": ba.ones((2, 4))}) == {0}
 
 
 def test_get_batch_axes_2_array() -> None:
     assert get_batch_axes(
-        (BatchedArray(np.ones((2, 3))), BatchedArray(np.ones((2, 3)), batch_axis=1)),
-        {"val": BatchedArray(np.ones((2, 3)))},
+        (ba.ones((2, 3), batch_axis=1), ba.ones(2)), {"batch": ba.ones((2, 4))}
     ) == {0, 1}
 
 
 def test_get_batch_axes_empty() -> None:
     assert get_batch_axes(tuple()) == set()
+
+
+###################################
+#     Tests for get_data_base     #
+###################################
+
+
+def test_get_data_base_true() -> None:
+    x = ba.ones((2, 3))
+    assert get_data_base(x) is x.base
+
+
+def test_get_data_base_true_slice() -> None:
+    x = ba.ones((2, 3))
+    assert get_data_base(x[1:]) is x.base
+
+
+def test_get_data_base_false() -> None:
+    x = ba.ones((2, 3))
+    assert get_data_base(x.copy()) is not x
